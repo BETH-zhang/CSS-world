d51c9e10d7795af73ab6fbbcb0819046b62d92c1
BETH-zhang <522718277@qq.com>
2019-06-19 19:28:56 +0800

    添加颜色转换的方法

---
+++ src/components/Color/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const colors = [
+  ''
+]
+
+export default class Color extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (<div className="colors-modal">
+
+    </div>)
+  }
+}
---
+++ src/utils/index.js
@@ -0,0 +1,154 @@
+export const randomColor = () => {
+  const r = parseInt(Math.random() * 256);
+  const g = parseInt(Math.random() * 256);
+  const b = parseInt(Math.random() * 256);
+  const rgb = "rgb("+r+","+g+","+b+")";
+  const rgb = `rgb(${r}, ${g}, ${b})`
+  return rgb;
+}
+
+export const rgbToHex = (r, g, b, a) => {
+  if (r <= 255 && g <= 255 && b <=255) {
+    rgbPToHex(r, g, b, a)
+  } else if (r.indexOf('rgb') > -1 && r.indexOf('%')) {
+    rgbPerToHex(r)
+  } else if (r.indexOf('rgb') > -1) {
+    rgbStrToHex(r)
+  }
+}
+
+// 255, 25, 2
+export const rgbPToHex = (r, g, b, a) => {
+  let red = r.toString(16)
+  let green = g.toString(16)
+  let blue = b.toString(16)
+  let opacity = a ? Math.round(a * 255).toString(16) : ''
+
+  if (red.length === 1) {
+    red = `0${red}`
+  }
+  if (green.length === 1) {
+    green = `0${green}`
+  }
+  if (blue.length === 1) {
+    blue = `0${blue}`
+  }
+  if (opacity && opacity.length === 1) {
+    opacity = `0${opacity}`
+  }
+  const hex = `#${red}${green}${blue}${opacity}`
+  return hex
+}
+
+// rgba(255, 25, 2)
+export const rgbStrToHex = (rgb) => {
+  // Choose correct separator
+  let sep = rgb.indexOf(",") > -1 ? "," : " ";
+  // Turn "rgb(r,g,b)" into [r,g,b]
+  rgb = rgb.substr(4).split(")")[0].split(sep);
+
+  let r = (+rgb[0]).toString(16),
+      g = (+rgb[1]).toString(16),
+      b = (+rgb[2]).toString(16);
+
+  if (r.length == 1)
+    r = "0" + r;
+  if (g.length == 1)
+    g = "0" + g;
+  if (b.length == 1)
+    b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
+// rgba(50%, 30%, 10%)
+// rgba(50% 30% 10%)
+export const rgbPerToHex = (rgba) => {
+  let sep = rgb.indexOf(",") > -1 ? "," : " ";
+  rgb = rgb.substr(4).split(")")[0].split(sep);
+
+  // Convert %s to 0–255
+  for (let R in rgb) {
+    let r = rgb[R];
+    if (r.indexOf("%") > -1)
+      rgb[R] = Math.round(r.substr(0,r.length - 1) / 100 * 255);
+      /* Example:
+      75% -> 191
+      75/100 = 0.75, * 255 = 191.25 -> 191
+      */
+  }
+}
+
+export const hexToRGB = (h) => {
+  let r = 0, g = 0, b = 0;
+  isPct = isPct === true;
+
+  if (h.length == 4) {
+    r = "0x" + h[1] + h[1];
+    g = "0x" + h[2] + h[2];
+    b = "0x" + h[3] + h[3];
+    
+  } else if (h.length == 7) {
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
+  }
+    
+  if (isPct) {
+    r = +(r / 255 * 100).toFixed(1);
+    g = +(g / 255 * 100).toFixed(1);
+    b = +(b / 255 * 100).toFixed(1);
+  }
+  
+  return "rgb(" + (isPct ? r + "%," + g + "%," + b + "%" : +r + "," + +g + "," + +b) + ")";
+}
+
+export const hexAToRGBA = (h) => {
+  let r = 0, g = 0, b = 0, a = 1;
+
+  if (h.length == 5) {
+    r = "0x" + h[1] + h[1];
+    g = "0x" + h[2] + h[2];
+    b = "0x" + h[3] + h[3];
+    a = "0x" + h[4] + h[4];
+
+  } else if (h.length == 9) {
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
+    a = "0x" + h[7] + h[8];
+  }
+  a = +(a / 255).toFixed(3);
+
+  return "rgba(" + +r + "," + +g + "," + +b + "," + a + ")";
+}
+
+// export const hexAToRGBA = (h,isPct) => {
+//   let r = 0, g = 0, b = 0, a = 1;
+//   isPct = isPct === true;
+    
+//   // Handling of digits
+
+//   if (isPct) {
+//     r = +(r / 255 * 100).toFixed(1);
+//     g = +(g / 255 * 100).toFixed(1);
+//     b = +(b / 255 * 100).toFixed(1);
+//   }
+//   a = +(a / 255).toFixed(3);
+
+//   return "rgba(" + (isPct ? r + "%," + g + "%," + b + "%," + a : +r + "," + +g + "," + +b + "," + a) + ")";
+// }
+
+// export const hexAToRGBA = (h,isPct) => {
+//   ...
+    
+//   a = +(a / 255).toFixed(3);
+//   if (isPct) {
+//     r = +(r / 255 * 100).toFixed(1);
+//     g = +(g / 255 * 100).toFixed(1);
+//     b = +(b / 255 * 100).toFixed(1);
+//     a = +(a * 100).toFixed(1);
+//   }
+
+//   return "rgba(" + (isPct ? r + "%," + g + "%," + b + "%," + a + "%" : +r + "," + +g + "," + +b + "," + a) + ")";
+// }
