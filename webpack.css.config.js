888c2c06acf3cb1d78fbec571c20cf8419edab7b
BETH-zhang <522718277@qq.com>
2019-06-27 19:28:26 +0800

    更新图片的输出引用路径

--- webpack.css.config.js
+++ webpack.css.config.js
@@ -77,6 +77,9 @@ module.exports = [
           loader: 'url-loader',
           options: {
             limit: 8192,
+            // name: '[name].[ext]',
+            outputPath: './',
+            publicPath: '../'
           }
         },
         {
